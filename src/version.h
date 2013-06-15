8cd4aee662088e18a267b05847bcdaf5804b0e6c
Jon Craton <jncraton@gmail.com>
2013-06-15 15:15:15 -0400

    added icon

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Automatically generated build information */
-#define VERSION "1.0.41"
-#define VERSION_COMMIT "88234935b87ef11c15eb2543799372ecedc5dda9"
+#define VERSION "1.0.42"
+#define VERSION_COMMIT "52f57408ac2e30a29c046afe6575bbaf9f2c3b71"
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_BUILD 41
+#define VERSION_BUILD 42
