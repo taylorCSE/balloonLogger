3b76b98710b3c90d19972567af434eeae55cd71b
Jon Craton <jncraton@gmail.com>
2013-12-18 13:26:38 -0500

    added version number display

--- src/BalloonLoggerCLI.c
+++ src/BalloonLoggerCLI.c
@@ -5,10 +5,15 @@
  * This is the entry point for the application.
  */
 
+#include <stdio.h>
+
 #include "logger.h"
 #include "comm.h"
+#include "version.h"
 
 int main() {
+    printf("BalloonLoggerCLI %s\n", VERSION);
+    
     COMM_open();
     
     while(1) {
--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Automatically generated build information */
-#define VERSION "1.0.357"
-#define VERSION_COMMIT "a6271ae63d108025467e5196c0f64c7b29f19cab"
+#define VERSION "1.0.365"
+#define VERSION_COMMIT "dc112b89d51c693ee41b812470cf98c1f59fbcbf"
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_BUILD 357
+#define VERSION_BUILD 365
